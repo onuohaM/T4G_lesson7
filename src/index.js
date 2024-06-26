2a921dd9f3627b46555f42755f8bb025516550aa
onuohaM <maryfeancisca@gmail.com>
2024-06-26 12:32:41 +0000

    adding all files

---
+++ src/index.js
@@ -0,0 +1,11 @@
+// alert('hi my name is Mary');
+// alert('i am 20 years');
+// alert('mostly Sad');
+let name1 = prompt('what is your name');
+
+// alert("Hi " + name + " , Welcome to our website");
+// alert(`Hi ${name1}, Welcome to our website`);
+let title = prompt('what is your title');
+// let name = ('what is your name');
+let favouriteproduct=prompt('what is your favourite product');
+alert(`hi ${title},${name1}, Do you like ${favouriteproduct}`);
